5a8d8c90aa22709709438959cd1418b239724269
Sam Judge <samuel.judge.92@gmail.com>
2020-01-18 22:26:39 +0000

    update

--- js/intermediate_algos/currying_argument_optional.js
+++ js/intermediate_algos/currying_argument_optional.js
@@ -1,16 +1,18 @@
 function addTogether(x) {
-    if( arguments.length === 2 && [...arguments].every( y => typeof y === 'number') ){
+    console.log(arguments[1])
+    if (arguments.length === 2 && [...arguments].every(y => typeof y === 'number')) {
         return x + arguments[1];
-    }else if( arguments.length === 1 && typeof arguments[0] === 'number' ){
-        return function(y){
-            if( typeof y === 'number'){
+    } else if (arguments.length === 1 && typeof arguments[0] === 'number') {
+        return function (y) {
+            console.log('y', y)
+            if (typeof y === 'number') {
                 return x + y;
             }
         }
     };
 }
 
-  
-  console.log(addTogether(2,'3'));
-  console.log(addTogether(2)('3'));
+
+// console.log(addTogether(2, 3));
+console.log(addTogether(2)(3));
 
